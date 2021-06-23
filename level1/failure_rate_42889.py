d93d8d8ec53e5cb500fe29436bfd86cc6acd4ca7
youngjinc <cyjin6789@gmail.com>
2021-06-23 14:38:58 +0900

    mod 21/6/23

--- level1/failure_rate_42889.py
+++ level1/failure_rate_42889.py
@@ -5,14 +5,14 @@ def solution(N, stages):
     for i in reversed(range(N)):
         stage = i+1 # 해당 스테이지
         count = stages.count(stage) # 실패율의 분자, 해당 스테이지에 도달한 플레이어 수
+        sum += count
         
-        if count != 0: # 실패율의 분자가 0이 아닌 경우
-            sum += count
+        if sum != 0: # 실패율의 분모가 0이 아닌 경우
             frate.append([count/sum, stage]) # [실패율, 스테이지]
-        else: # 실패율의 분자가 0인 경우
+        else: # 실패율의 분모가 0인 경우
             frate.append([0, stage])
 
-    frate = sorted(frate, key = lambda x : (-x[0], x[1])) # 실패율 내림차순, 실패율이 동일한 경우 스테이지 오름차순 정렬
+    frate = sorted(frate, key = lambda x : (-x[0], x[1])) # 실패율 내림차순, 실패율이 동일한 경우 인덱스 오름차순 정렬
     return [frate[i][1] for i in range(N)]
 
-print(solution(5,[2, 1, 2, 6, 2, 4, 3, 3])) # 테스트 코드
+# print(solution(5,[2, 1, 2, 6, 2, 4, 3, 3]))
