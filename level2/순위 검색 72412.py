2660cfe4a1ba146a8529dbd828f9d6f69e6d6093
youngjinc <cyjin6789@gmail.com>
2021-07-11 23:59:08 +0900

    update 21/7/11

--- level2/순위 검색 72412.py
+++ level2/순위 검색 72412.py
@@ -18,4 +18,55 @@ def solution1(info, query): # 정확성 통과 O, 효율성 통과 X
                 answer[q] += 1
 
     return answer
-print(solution1(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"], ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))
+
+def lowerboundSearch(arr, target):
+    if len(arr) == 0 or (len(arr) >= 1 and target > arr[-1]): 
+        return len(arr) 
+    left, right = 0, len(arr) - 1 
+    while (left < right): 
+        mid = (left + right) // 2 
+        if(arr[mid] < target): 
+            left = mid + 1 
+        else:
+            right = mid 
+    return right
+    
+def parseQuery(s): 
+    string = s.replace("and ", "").split(" ") 
+    number = int(string[-1]) 
+    return ''.join(string[:-1]), number 
+    
+def parseInfo(info): 
+    arr = info.split(' ') 
+    keys = [] 
+    for i in (arr[0], '-'): 
+        for j in (arr[1], '-'): 
+            for k in (arr[2], '-'): 
+                for l in (arr[3], '-'): 
+                    keys.append(i+j+k+l) 
+    return keys, int(arr[-1]) 
+
+def solution2(info, query): 
+    answer = [] 
+    dic = {} 
+    for l in ('cpp', 'java', 'python', '-'): 
+        for i in ('backend', 'frontend', '-'): 
+            for s in ('senior', 'junior', '-'): 
+                for f in ('pizza', 'chicken', '-'): 
+                    dic[l+i+s+f] = [] 
+    for l in info: 
+        keys, score = parseInfo(l) 
+        for key in keys: 
+            dic[key].append(score) 
+    
+    for k in dic.keys(): 
+        dic[k].sort() 
+    
+    for i in query: 
+        q, num = parseQuery(i) 
+        scores = dic[q] 
+        answer.append(len(scores) - lowerboundSearch(scores, num)) 
+    return answer
+
+#테스트 코드
+print(solution2(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"], ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))
