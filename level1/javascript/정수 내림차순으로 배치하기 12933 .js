b6f5e4f57c887ad42136f483624abc8d7fbfd973
youngjinc <cyjin6789@gmail.com>
2021-07-22 22:21:20 +0900

    21/7/22

--- level1/javascript/정수 내림차순으로 배치하기 12933 .js
+++ level1/javascript/정수 내림차순으로 배치하기 12933 .js
@@ -1,14 +1,14 @@
 function solution1(n) { // 문자 풀이
-    return Number((n+'').split('').sort((a,b)=>b-a).join(''));
+    return (n+'').split('').sort((a,b)=>b-a).join('')*1; // *1 : 문자열 -> 숫자 형변환
 }
 
 function solution2(n) { // 숫자 풀이
     let nums = []
     do{
         nums.push(n % 10);
-        n = parseInt(n / 10);
+        n = Math.floor(n / 10); // parseInt보다 Math.floor이 실행 속도 더 빠름
     }while(n > 0)
-    return nums.sort((a,b)=>b-a).join('')*1; // *1 : 문자열 -> 숫자 형변환
+    return nums.sort((a,b)=>b-a).join('')*1; 
 }
 
 // 테스트 코드
