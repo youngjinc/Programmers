46bd70218eea97a63fdcf34001f3c950a0a8d17a
youngjinc <cyjin6789@gmail.com>
2021-08-08 21:28:02 +0900

    Update 예상 대진표 12985.js

--- level2/javascript/예상 대진표 12985.js
+++ level2/javascript/예상 대진표 12985.js
@@ -1,18 +1,15 @@
 function solution1(n,a,b)
 {
-    let round = 1;
+    let round = 0;
 
-    while(true){
-        a = Math.ceil(a/2);
-        b = Math.ceil(b/2);
-        
-        if(a == b)
-            break;
-        else
-            round++;
+    while(a != b){
+        round++;
+        a = Math.ceil(a/2); // a = (a+1) >> 1 도 가능
+        b = Math.ceil(b/2); // b = (b+1) >> 1 도 가능 
+        console.log(a,b)
     }
     return round;
 }
 
 // 테스트 코드
-console.log(solution1(8,4,7));
+console.log(solution2(8,4,7));
